b1b80c628ad8fe4e626c852e2863671f5faec90b
Divyanshu gupta <gupta.divyanshuu@gmail.com>
2022-01-08 01:36:44 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'season roof fragile cherry joy economy then release alley saddle struggle drive',
+  'https://rinkeby.infura.io/v3/030b81b1858c4091a5355508b62bf572'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Divyanshu'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
